2bcdc19d65dd94ede9935996a736155c4358ef25
kratikgarg09 <gargkratik707@gmail.com>
2025-06-11 07:37:13 +0530

    come patch fixes and bugfixes

--- src/main/java/com/kratik/task_manager/model/TagsEntity.java
+++ src/main/java/com/kratik/task_manager/model/TagsEntity.java
@@ -1,7 +1,6 @@
 package com.kratik.task_manager.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,4 +13,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class TagsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
 }
